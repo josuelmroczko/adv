d786e2bb2f223ad51c1e196f15d9268667213239
josuel <josuelmroczko>
2024-03-12 15:31:19 -0300

    a

--- src/componentes/footer/footer.jsx
+++ src/componentes/footer/footer.jsx
@@ -1,9 +1,12 @@
+import { data } from "../data";
+import { StyleFooter } from "./styledFooter";
+
 export default function Footer(){
     return(
-        <>
+        <StyleFooter>
         <div>
-            <h1>Rodolfo Reis Advogados</h1>
-            <p>Escritório de Advocacia que atua nas áreas Tributária, Empresarial, Trabalhista, Civil, Família e Sucessões, numa estrutura enxuta, com profissionais especializados, que lidam pessoalmente nas lides.</p>
+            <h1>{data.fotter.titulo1}</h1>
+            <p>{data.fotter.texto}</p>
             <div>
                 <ul>
                     <li><img src="" alt="" /></li>
@@ -14,30 +17,30 @@ export default function Footer(){
             </div>
 
             <div>
-                <h2>Visite-Nos</h2>
+                <h2>{data.fotter.titulo2}</h2>
                 <ul>
-                    <li><a href=""></a></li>
-                    <li><a href=""></a></li>
-                    <li><a href=""></a></li>
-                    <li><a href=""></a></li>
-                    <li><a href=""></a></li>
+                    <li><a href=""> {data.fotter.semana}</a></li>
+                    <li><a href="">{data.fotter.endereco}</a></li>
+                    <li><a href="">{data.fotter.telefone}</a></li>
+                    <li><a href="">{data.fotter.whatsapp}</a></li>
+                    <li><a href="">{data.fotter.email}</a></li>
                 </ul>
             </div>+
 
             <div>
                 <h2>Areas de atuaçao</h2>
                 <ul>
-                    <li>as</li>
-                    <li>asas</li>
-                    <li>aas</li>
-                    <li>as</li>
-                    <li>a</li>
-                    <li>a</li>
-                    <li>a</li>
-                    <li>a</li>
+                    <li>{data.fotter.texto1}</li>
+                    <li>{data.fotter.texto2}</li>
+                    <li>{data.fotter.texto3}</li>
+                    <li>{data.fotter.texto4}</li>
+                    <li>{data.fotter.texto5}</li>
+                    <li>{data.fotter.texto6}</li>
+                    <li>{data.fotter.texto7}</li>
+                    <li></li>
                 </ul>
             </div>
         </div>
-        </>
+        </StyleFooter>
     )
 }
---
+++ src/componentes/footer/styledFooter.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const StyleFooter = styled.footer`
+display: flex;
+`
